9e56d3dd8e51d283c40d5d5d54b05d7d1bc4a135
bwold001 <bezamoges@hotmail.com>
2017-12-14 19:59:22 -0500

    Add files via upload
    number of eggs

---
+++ Eggs/Eggs/frmEggs.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEggs
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstBoxEggs = New System.Windows.Forms.ListBox()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstBoxEggs
+        '
+        Me.lstBoxEggs.FormattingEnabled = True
+        Me.lstBoxEggs.Location = New System.Drawing.Point(22, 86)
+        Me.lstBoxEggs.Name = "lstBoxEggs"
+        Me.lstBoxEggs.Size = New System.Drawing.Size(233, 134)
+        Me.lstBoxEggs.TabIndex = 0
+        '
+        'btnProcess
+        '
+        Me.btnProcess.Location = New System.Drawing.Point(32, 12)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(208, 56)
+        Me.btnProcess.TabIndex = 1
+        Me.btnProcess.Text = "Process Eggs"
+        Me.btnProcess.UseVisualStyleBackColor = True
+        '
+        'frmEggs
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(275, 232)
+        Me.Controls.Add(Me.btnProcess)
+        Me.Controls.Add(Me.lstBoxEggs)
+        Me.Name = "frmEggs"
+        Me.Text = "Eggs"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstBoxEggs As System.Windows.Forms.ListBox
+    Friend WithEvents btnProcess As System.Windows.Forms.Button
+
+End Class
---
+++ Eggs/Eggs/frmEggs.vb
@@ -0,0 +1,60 @@
+﻿Public Class frmEggs
+
+    Dim eggs() As String = IO.File.ReadAllLines("Eggs.txt")
+    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
+
+        Dim jumbo, extraLarge, large, medium, small As Integer
+        count(jumbo, extraLarge, large, medium, small)
+        display(jumbo, extraLarge, large, medium, small)
+
+    End Sub
+
+    Sub count(ByRef jumbo As Integer, ByRef extraLarge As Integer, ByRef large As Integer, ByRef medium As Integer, ByRef small As Integer)
+        For i As Integer = 0 To eggs.Count - 1
+            If eggs(i) >= 2.5 Then
+                jumbo += 1
+            ElseIf eggs(i) >= 2.25 And eggs(i) < 2.5 Then
+                extraLarge += 1
+            ElseIf eggs(i) >= 2 And eggs(i) < 2.25 Then
+                large += 1
+            ElseIf eggs(i) >= 1.75 And eggs(i) < 2 Then
+                medium += 1
+            ElseIf eggs(i) >= 1.5 And eggs(i) < 1.75 Then
+                small += 1
+            End If
+        Next
+    End Sub
+
+    Function calculateLightest()
+        Dim lightest As Double = eggs(0)
+        For i As Integer = 0 To eggs.Count - 1
+            If eggs(i) < lightest Then
+                lightest = eggs(i)
+            End If
+        Next
+        Return lightest
+    End Function
+
+    Function calculateHeaviest()
+        Dim heaviest As Double = eggs(0)
+        For i As Integer = 0 To eggs.Count - 1
+            If eggs(i) > heaviest Then
+                heaviest = eggs(i)
+            End If
+        Next
+        Return heaviest
+    End Function
+
+    Sub display(ByRef jumbo As Integer, ByRef extraLarge As Integer, ByRef large As Integer, ByRef medium As Integer, ByRef small As Integer)
+
+        lstBoxEggs.Items.Add(jumbo & "  Jumbo eggs")
+        lstBoxEggs.Items.Add(extraLarge & "  Extra Large eggs")
+        lstBoxEggs.Items.Add(large & "  Large eggs")
+        lstBoxEggs.Items.Add(medium & "  Medium eggs")
+        lstBoxEggs.Items.Add(small & "  Small eggs")
+        lstBoxEggs.Items.Add("Lightest egg: " & calculateLightest() & "  ounces")
+        lstBoxEggs.Items.Add("Heaviest egg: " & calculateHeaviest() & "  ounces")
+
+    End Sub
+
+End Class
