8b3c37d3cd9e15e3ef058c175d513a341b4359c6
bwold001 <bezamoges@hotmail.com>
2017-12-14 19:56:01 -0500

    Add files via upload
    score frequencies

---
+++ Frequencies/Frequencies/frmFrequency.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFrequency
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstBoxDisplay = New System.Windows.Forms.ListBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstBoxDisplay
+        '
+        Me.lstBoxDisplay.FormattingEnabled = True
+        Me.lstBoxDisplay.Location = New System.Drawing.Point(12, 75)
+        Me.lstBoxDisplay.Name = "lstBoxDisplay"
+        Me.lstBoxDisplay.Size = New System.Drawing.Size(206, 95)
+        Me.lstBoxDisplay.TabIndex = 0
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(12, 26)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(206, 29)
+        Me.btnDisplay.TabIndex = 1
+        Me.btnDisplay.Text = "Display Frequencies"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'frmFrequency
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(237, 190)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.lstBoxDisplay)
+        Me.Name = "frmFrequency"
+        Me.Text = "Frequency"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lstBoxDisplay As System.Windows.Forms.ListBox
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+
+End Class
---
+++ Frequencies/Frequencies/frmFrequency.vb
@@ -0,0 +1,53 @@
+﻿Public Class frmFrequency
+
+    Dim scores() As String = IO.File.ReadAllLines("Scores.txt")
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        lstBoxDisplay.Items.Add("Intervals" & ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "Frequency")
+        lstBoxDisplay.Items.Add("0 to 9" & ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & frequencies(0, scores))
+        lstBoxDisplay.Items.Add("10 to 19" & ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & frequencies(1, scores))
+        lstBoxDisplay.Items.Add("20 to 29" & ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & frequencies(2, scores))
+        lstBoxDisplay.Items.Add("30 to 39" & ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & frequencies(3, scores))
+        lstBoxDisplay.Items.Add("40 to 49" & ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & frequencies(4, scores))
+    End Sub
+
+    Function frequencies(num As Integer, data() As String) As Integer
+        Dim count As Integer = 0
+        Select Case num
+            Case 0
+                For i As Integer = 0 To data.Count() - 1
+                    If data(i) < 10 Then
+                        count += 1
+                    End If
+                Next
+                Return count
+            Case 1
+                For i As Integer = 0 To data.Count() - 1
+                    If data(i) >= 10 And data(i) < 20 Then
+                        count += 1
+                    End If
+                Next
+                Return count
+            Case 2
+                For i As Integer = 0 To data.Count() - 1
+                    If data(i) >= 20 And data(i) < 30 Then
+                        count += 1
+                    End If
+                Next
+                Return count
+            Case 3
+                For i As Integer = 0 To data.Count() - 1
+                    If data(i) >= 30 And data(i) < 40 Then
+                        count += 1
+                    End If
+                Next
+                Return count
+            Case 4
+                For i As Integer = 0 To data.Count() - 1
+                    If data(i) >= 40 And data(i) < 50 Then
+                        count += 1
+                    End If
+                Next
+                Return count
+        End Select
+    End Function
+End Class
